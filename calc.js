dc5e0944b474618065bbf9a7651bd6907c503046
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2021-12-30 19:25:27 +0545

    variable example calc

---
+++ calc.js
@@ -0,0 +1,39 @@
+<script type="text/javascript">
+	
+	var a;
+	a = 10;
+	var b =5;
+	var calc = "Calculation"
+	var add = a + b;
+	var sub = a - b;
+	var mul = a * b;
+	var div = a / b;
+	var mod = a % b;
+
+	// alert(calc);
+	
+	document.write(calc);
+	document.write("<br>");
+	document.write("a=10 and b=5");
+	document.write("<br><hr>");
+	document.write("Addition:");
+	document.write(add);
+	document.write("<br>");
+	document.write("Subtraction:");
+	document.write(sub);
+	document.write("<br>");
+	document.write("Multiplication:");
+	document.write(mul);
+	document.write("<br>");
+	document.write("Division:");
+	document.write(div);
+	document.write("<br>");
+	document.write("Reminder:");
+	document.write(mod);
+	document.write("<br>");
+
+</script>
+
+
+
+
